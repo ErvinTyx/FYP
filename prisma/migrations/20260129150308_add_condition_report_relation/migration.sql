ac6c8035be9954f53b997f90c3c3726c0ad1c4be
Wiston0004 <wistonchiang04@gmail.com>
2026-02-04 15:29:05 +0800

    Make changes all migration fix

--- prisma/migrations/20260129150308_add_condition_report_relation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `OpenRepairSlip` ADD CONSTRAINT `OpenRepairSlip_conditionReportId_fkey` FOREIGN KEY (`conditionReportId`) REFERENCES `ConditionReport`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
